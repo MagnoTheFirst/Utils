afcd562b31d5734b789da8c9df879209fd3cb675
Laneri Alejandro, INI-DOS-EAS <Alejandro.Laneri1@swisscom.com>
2021-06-30 11:20:24 +0200

    Unit Tests

--- tests/moduls/File_handlerTest.java
+++ tests/moduls/File_handlerTest.java
@@ -19,6 +19,7 @@ class File_handlerTest {
 	File_handler f;
 	File_handler f1;
 	File_handler f2;
+	File_handler f3;
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		FileWriter file = new FileWriter("C:/Users/taalaal4/LAB/test_cases.txt");
@@ -32,6 +33,10 @@ class File_handlerTest {
 		FileWriter file2 = new FileWriter("C:/Users/taalaal4/LAB/test_cases2.txt");
 		file2.write("Test Cases\n<html>\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>");
 		file2.close();
+		
+		FileWriter file3 = new FileWriter("C:/Users/taalaal4/LAB/test_cases3.txt");
+		file3.write("Test Cases\n<html>\n\n\n\n</html>");
+		file3.close();
 				
 	}
 
@@ -45,6 +50,9 @@ class File_handlerTest {
 		
 		File file2 = new File("C:/Users/taalaal4/LAB/test_cases2.txt");
 		file1.delete();
+		
+		File file3 = new File("C:/Users/taalaal4/LAB/test_cases3.txt");
+		file1.delete();
 	}
 
 	@BeforeEach
@@ -52,6 +60,7 @@ class File_handlerTest {
 		f = new File_handler("C:/Users/taalaal4/LAB/", "test_cases.txt");
 		f1 = new File_handler("C:/Users/taalaal4/LAB/", "test_cases1.txt");
 		f2 = new File_handler("C:/Users/taalaal4/LAB/", "test_cases2.txt");
+		f3 = new File_handler("C:/Users/taalaal4/LAB/", "test_cases3.txt");
 	}
 
 	@AfterEach
@@ -62,80 +71,65 @@ class File_handlerTest {
 	@Test
 	@DisplayName("Constructor")
 	void testFile_handler() {
-		assertEquals("C:/Users/taalaal4/LAB/", f1.getPath());
-		assertEquals("test_cases.txt", f1.getFilename());
-		assertEquals(true, f1.getSource_file().exists());
+		assertEquals("C:/Users/taalaal4/LAB/", f.getPath());
+		assertEquals("test_cases.txt", f.getFilename());
+		assertEquals(true, f.getSource_file().exists());
 	}
 
 	@Test
 	@DisplayName("Should return an ArrayList<String> each index equals a line from text_cas.txt")
 	void testGet_Lines_of_File() {
 
-		assertEquals("Test Cases",f1.get_Lines_of_File().get(0));
-		assertEquals("<html>",f1.get_Lines_of_File().get(1));
-		assertEquals("</html>",f1.get_Lines_of_File().get(f1.get_Lines_of_File().size()-1));
+		assertEquals("Test Cases",f.get_Lines_of_File().get(0));
+		assertEquals("<html>",f.get_Lines_of_File().get(1));
+		assertEquals("</html>",f.get_Lines_of_File().get(f1.get_Lines_of_File().size()-1));
 	}
 	
 	@Test
 	void testReplace_line_content() {
-		assertEquals("Test Cases\n111\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>",f1.replace_line_content("<html>", "111"));
-		assertEquals("Test Cases\n111\n222\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>",f1.replace_line_content("<head>", "222"));
-		assertEquals("Test Cases\n111\n222\n333\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>",f1.replace_line_content("</head>", "333"));
+		assertEquals("Test Cases\n111\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>",f.replace_line_content("<html>", "111"));
+		assertEquals("Test Cases\n111\n222\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>",f.replace_line_content("<head>", "222"));
+		assertEquals("Test Cases\n111\n222\n333\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>",f.replace_line_content("</head>", "333"));
 		
 	}
 
 	@Test
 	void testGet_specific_line() {
-		assertEquals("<html>", f1.get_specific_line(1));
-		assertEquals("<head>", f1.get_specific_line(2));
-		assertEquals("<h1>test case</h1>", f1.get_specific_line(5));
+		assertEquals("<html>", f.get_specific_line(1));
+		assertEquals("<head>", f.get_specific_line(2));
+		assertEquals("<h1>test case</h1>", f.get_specific_line(5));
 	}
 	@Test
 	void testFile_writer() {
-		assertEquals("Test Cases\n<html>\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>\nK: Hello There!",f2.file_writer("K: Hello There!"));
-		assertEquals("Test Cases\n<html>\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>\nK: Hello There!\nG: General Kenobiiii",f2.file_writer("G: General Kenobiiii"));
+		assertEquals("Test Cases\n<html>\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>\nK: Hello There!",f1.file_writer("K: Hello There!"));
+		assertEquals("Test Cases\n<html>\n<head>\n</head>\n<body>\n<h1>test case</h1>\n\n\n<p>lorem ipsum</p>\n</body>\n</html>\nK: Hello There!\nG: General Kenobiiii",f1.file_writer("G: General Kenobiiii"));
 	}
 
 
 	@Test
 	void testOverwrite_file() {
-		assertEquals("HelloWorld", );
-//	}
-//
-//	@Test
-//	void testClear_file() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	void testRemove_empty_lines() {
-//		fail("Not yet implemented");
-//	}
-//	
-//	@Test
-//	void testGetPath() {
-//
-//	}
-//	@Test
-//	void testSetPath() {
-//
-//	}
-//	@Test
-//	void getFilename() {
-//	
-//	}
-//	@Test
-//	void setFilename() {
-//
-//	}
-//	@Test
-//	void getSource_file() {
-//
-//	}
-//	@Test
-//	void setSource_file() {
-//
-//	}
-//	
+		assertEquals("HelloWorld", f2.overwrite_file("HelloWorld"));
+	}
+
+	@Test
+	void testClear_file() {
+		assertEquals("", f2.clear_file());
+	}
+
+	@Test
+	void testRemove_empty_lines() {
+		assertEquals("Test Cases\n<html>\n</html>\n", f3.remove_empty_lines());
+	}
+
+	@Test
+	void testGetPath() {
+		assertEquals("C:/Users/taalaal4/LAB/", f1.getPath());
+
+	}
+	@Test
+	void getFilename() {
+		assertEquals("test_cases1.txt", f1.getFilename());
+	}
+
 
 }
